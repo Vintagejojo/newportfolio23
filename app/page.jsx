e4cb634b3a9c7c5e7f63f61a498d3e6b1748a789
Vintagejojo <JDnal7@gmail.com>
2023-05-17 16:06:21 -0500

    Hero component
    DRY-Don't Repeat Yourself

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,10 @@
-
+import Hero from "@/components/Hero"
 
 
 export default function Homepage() {
     return (
-        <div className="border-2 border-dotted h-screen w-auto my-15">
-            <div className="border-2 border-dotted flex flex-col w-2/4 m-auto mt-20">
-                <h1 className="text-7xl pb-5">Vintagejojo</h1>
-                <span className="text-2xl">Full Stack Developer</span>
-                <p className="text-lg">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, illum non! Quisquam praesentium tenetur vitae enim doloribus dicta repellendus quo voluptatem maxime voluptate nostrum, similique ea perspiciatis veniam. Omnis, error?</p>
-            </div>
-        </div>
+        <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+            <Hero />
+        </main>
     )
 }
--- components/Hero.jsx
+++ components/Hero.jsx
@@ -1,18 +1,41 @@
+"use client"
 import React from "react";
 import {Link} from "react-scroll/modules"
 import Image from "next/image";
+import {HiArrowDown} from "react-icons/hi"
+
 
 export default function Hero() {
     return (
         <section>
-            <div>
-                <div>
-                    
+            <div className="flex flex-col text-center items-center justify-center my-10 py-10 sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-52">
+                <div className="md:w-1/2 md:mt-2" >
+                    <Image className="rounded-full" src="/jopic.jpg" alt="my head" width={300} height={300} />    
                 </div>
-                <div>
-
+                <div className="md:mt-2 md:w-3/5">
+                    <h1 className="font-bold text-4xl mt-6 md:text-7xl md:mt-0">Hello, I'm Jordan.</h1>
+                    <p className="text-lg mt-4 mb-6 md:text-2xl">
+                        I'm a{" "}
+                        <span className="font-semibold text-teal-600">
+                            Full Stack Engineer{" "}
+                        </span>
+                        descending into madness....one line of code at a time
+                    </p>
                 </div>
+                
             </div>
+            <div className="flex flex-row justify-center">
+                    <Link
+                        to="about"
+                        activeClass="active"
+                        spy={true}
+                        smooth={true}
+                        offset={-100}
+                        duration={500}
+                    >
+                        <HiArrowDown size={35}/>
+                    </Link>
+                </div>
         </section>
     )
 }
