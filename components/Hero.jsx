6d896ec71a662144a2f06ec6cac763c562f21c76
Vintagejojo <JDnal7@gmail.com>
2023-05-18 09:58:04 -0500

    newportfolio23v0.5
    Finishing touches to hero section. Minor tweaks to tsconfig

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,7 +8,7 @@ import {HiArrowDown} from "react-icons/hi"
 export default function Hero() {
     return (
         <section>
-            <div className="flex flex-col text-center items-center justify-center my-10 py-10 sm:py-32 md:flex-row md:space-x-4 md:text-left md:py-52">
+            <div className="flex flex-col text-center items-center justify-center my-6 py-9 sm:py-30 md:flex-row md:space-x-4 md:text-left md:py-40">
                 <div className="md:w-1/2 md:mt-2" >
                     <Image className="rounded-full" src="/jopic.jpg" alt="my head" width={300} height={300} />    
                 </div>
@@ -33,7 +33,7 @@ export default function Hero() {
                         offset={-100}
                         duration={500}
                     >
-                        <HiArrowDown size={35}/>
+                        <HiArrowDown size={35} className="animate-bounce"/>
                     </Link>
                 </div>
         </section>
